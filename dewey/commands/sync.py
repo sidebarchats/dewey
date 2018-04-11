4811a2999c9e46794d7825cda1191d9325b7ef9b
Steven Skoczen <steven@quantumimagery.com>
2018-04-11 20:14:40 +1200

    Adds git branch status.

---
+++ dewey/commands/sync.py
@@ -0,0 +1,17 @@
+import subprocess
+from clint.textui import puts, indent, colored
+
+from .base import DeweyCommand
+from dewey.util import suppress_stdout_stderr
+
+
+class Command(DeweyCommand):
+
+    def pre_default(self, *args, **kwargs):
+        return 'git pull'
+
+    def run_command(self, *args, **kwargs):
+        pass
+
+    def post_default(self, *args, **kwargs):
+        return '~/.pyenv/versions/2.7/lib/python2.7/site-packages/dewey/bin/git-branch-status.sh'
