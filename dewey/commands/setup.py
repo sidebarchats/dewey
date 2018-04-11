ab7f61313ff76c9cc9636f40e3b682fb660c51c4
Steven Skoczen <steven@quantumimagery.com>
2018-04-11 18:48:28 +1200

    Pulls cruft, adds npm.

--- dewey/commands/setup.py
+++ dewey/commands/setup.py
@@ -14,12 +14,15 @@ class Command(DeweyCommand):
         # return 'docker-compose --project-name bu run db bash -c "createdb -h db -U postgres sidebar"'
 
     def run_command(self, *args, **kwargs):
+        puts("Checking npm...",)
         if os.path.isfile("package.json"):
+            puts(" found.  Installing...")
             subprocess.call("npm install", shell=True)
+            puts("Installed.")
         if os.path.isfile("requirements.unstable.txt"):
             subprocess.call("pip install -r requirements.unstable.txt", shell=True)
         elif os.path.isfile("requirements.txt"):
             subprocess.call("pip install -r requirements.txt", shell=True)
 
     def post_default(self, *args, **kwargs):
-        return 'cd app/api; python manage.py migrate'
+        pass
