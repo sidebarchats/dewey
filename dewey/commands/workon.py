04e76f082ef7f344a20036fe548b80c4bc38062a
Steven Skoczen <steven@quantumimagery.com>
2018-04-11 19:52:00 +1200

    Adds formatting.

--- dewey/commands/workon.py
+++ dewey/commands/workon.py
@@ -4,7 +4,7 @@ from .base import DeweyCommand
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        return "workon sidebar-%(app_name)s" % kwargs
+        return "workon sidebar-%s" % kwargs["<app_name>"]
 
     def run_command(self, *args, **kwargs):
         pass
