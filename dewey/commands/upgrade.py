8382e9a00b94353bed2de59923421b4edc6188f0
Steven Skoczen <steven@quantumimagery.com>
2018-04-11 20:33:04 +1200

    Handles venv properly.

--- dewey/commands/upgrade.py
+++ dewey/commands/upgrade.py
@@ -1,3 +1,4 @@
+import
 import subprocess
 from .base import DeweyCommand
 from dewey.util import suppress_stdout_stderr
@@ -5,16 +6,26 @@ from dewey.util import suppress_stdout_stderr
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
+        self.venv = False
+        if "VIRTUAL_ENV" in os.environ:
+            self.venv = os.envrion["VIRTUAL_ENV"].split("/")[-1].replace("sidebar-", "")
+
         return "printf 'Upgrading dewey... '"
 
     def run_command(self, *args, **kwargs):
             try:
                 with suppress_stdout_stderr():
-                    output = subprocess.check_output("bash -c 'pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force'", shell=True,)
+                    if self.venv:
+                        output = subprocess.check_output("deactivate; pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force", shell=True,)
+                    else:
+                        output = subprocess.check_output("pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force", shell=True,)
                 print("complete.")
             except subprocess.CalledProcessError as grepexc:                                                                                                   
                 print("\nError upgrading dewey. \n%s" % (grepexc.output,))
 
 
     def post_default(self, *args, **kwargs):
-        return "source ~/.pyenv/versions/2.7/lib/python2.7/site-packages/dewey/bin/bootstrap_dewey.sh"
+        if self.venv:
+            return "d workon %s; source ~/.pyenv/versions/2.7/lib/python2.7/site-packages/dewey/bin/bootstrap_dewey.sh" % self.venv
+        else:
+            return "source ~/.pyenv/versions/2.7/lib/python2.7/site-packages/dewey/bin/bootstrap_dewey.sh"
