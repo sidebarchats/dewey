4cf934153209a82ee5a3ab630411618f9385c37f
Steven Skoczen <steven@quantumimagery.com>
2018-04-16 13:40:03 +1200

    Adds docker support

--- dewey/commands/setup.py
+++ dewey/commands/setup.py
@@ -12,28 +12,34 @@ class Command(DeweyCommand):
         pass
 
     def run_command(self, *args, **kwargs):
-        puts("Checking nodejs requirements...", newline=False)
-        if os.path.isfile("package.json"):
-            puts(" found.")
-            puts("Installing package.json...", newline=False)
-            subprocess.call("npm install", shell=True)
+        puts("Checking docker...", newline=False)
+        if os.path.isfile("docker-compose.yml"):
+            subprocess.call("docker-compose build; docker-compose up")
             puts(" done.")
         else:
-            puts(" package.json not found.")
+            puts("docker not found.")
+            puts("Checking nodejs requirements...", newline=False)
+            if os.path.isfile("package.json"):
+                puts(" found.")
+                puts("Installing package.json...", newline=False)
+                subprocess.call("npm install", shell=True)
+                puts(" done.")
+            else:
+                puts(" package.json not found.")
 
-        puts("Checking python requirements...", newline=False)
-        if os.path.isfile("requirements.unstable.txt"):
-            puts(" found.")
-            puts("Installing requirements.unstable.txt...", newline=False)
-            subprocess.call("pip install -r requirements.unstable.txt", shell=True)
-            puts(" done.")
-        elif os.path.isfile("requirements.txt"):
-            puts(" found.")
-            puts("Installing requirements.txt...", newline=False)
-            subprocess.call("pip install -r requirements.txt", shell=True)
-            puts(" done.")
-        else:
-            puts(" no python requirements.txt files found.")
+            puts("Checking python requirements...", newline=False)
+            if os.path.isfile("requirements.unstable.txt"):
+                puts(" found.")
+                puts("Installing requirements.unstable.txt...", newline=False)
+                subprocess.call("pip install -r requirements.unstable.txt", shell=True)
+                puts(" done.")
+            elif os.path.isfile("requirements.txt"):
+                puts(" found.")
+                puts("Installing requirements.txt...", newline=False)
+                subprocess.call("pip install -r requirements.txt", shell=True)
+                puts(" done.")
+            else:
+                puts(" no python requirements.txt files found.")
 
     def post_default(self, *args, **kwargs):
         pass
