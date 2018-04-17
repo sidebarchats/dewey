9be1940c0f17007105c2ef8fa53930f7a9b8fa72
Steven Skoczen <steven@quantumimagery.com>
2018-04-17 19:18:52 +1200

    Fixes upgrade command.

--- dewey/commands/upgrade.py
+++ dewey/commands/upgrade.py
@@ -10,8 +10,8 @@ class Command(DeweyCommand):
     def run_command(self, *args, **kwargs):
             try:
                 with suppress_stdout_stderr():
-                    output = subprocess.check_output("bash -c 'pyenv 3.6.5 && pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force'", shell=True,)
-                    output = subprocess.check_output("bash -c 'pyenv 2.7.7 && pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force'", shell=True,)
+                    output = subprocess.check_output("bash -c 'pyenv local 3.6.5 && pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force'", shell=True,)
+                    output = subprocess.check_output("bash -c 'pyenv local 2.7.7 && pip install git+https://git@github.com/sidebarchats/dewey.git#egg=dewey --upgrade --force'", shell=True,)
                 print("complete.")
             except subprocess.CalledProcessError as grepexc:                                                                                                   
                 print("\nError upgrading dewey. \n%s" % (grepexc.output,))
