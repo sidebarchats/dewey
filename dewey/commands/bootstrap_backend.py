1554ce4ea890e9f43ead404d990a414e6df141af
Steven Skoczen <steven@quantumimagery.com>
2018-04-18 12:05:24 +1200

    Adds in ngrok support.

--- dewey/commands/bootstrap_backend.py
+++ dewey/commands/bootstrap_backend.py
@@ -1,3 +1,6 @@
+import os
+import json
+import requests
 import subprocess
 from clint.textui import puts, indent, colored
 
@@ -8,9 +11,18 @@ from dewey.util import suppress_stdout_stderr
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        pass
+        return "ngrok http 8120 &> /dev/null &"
 
     def run_command(self, *args, **kwargs):
+        puts("Verifying tunnel...", newline=False)
+        r = requests.get("http://localhost:4040/api/tunnels")
+        j = r.json()
+        if "tunnels" in j:
+            self.brain.api_url = j["tunnels"]["public_url"]
+            self.brain.save()
+            os.environ["SIDEBAR_API_URL"] = self.brain.api_url
+            puts(" found at %s." % self.brain.api_url)
+
         puts("Bootstrapping backend...", newline=False)
         subprocess.call("cd ../backend && docker-compose up -d --no-recreate", shell=True)
         puts(" done.")
