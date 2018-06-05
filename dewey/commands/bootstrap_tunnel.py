ef77d317aa8a99663524ca99ee8d67d41ff2da24
Steven Skoczen <steven@quantumimagery.com>
2018-06-05 17:37:52 +0200

    Teaches dewey to more robustly handle tunnel bootstraps.

--- dewey/commands/bootstrap_tunnel.py
+++ dewey/commands/bootstrap_tunnel.py
@@ -15,13 +15,35 @@ class Command(DeweyCommand):
 
     def run_command(self, *args, **kwargs):
         puts("Verifying tunnel...", newline=False)
-        r = requests.get("http://localhost:4040/api/tunnels")
-        j = r.json()
-        if "tunnels" in j:
-            self.brain.api_url = j["tunnels"][0]["public_url"]
-            self.save()
-            os.environ["SIDEBAR_API_URL"] = self.brain.api_url
-            puts(" found at %s." % self.brain.api_url)
+        retries = 0
+        while retries < 2:
+            r = requests.get("http://localhost:4040/api/tunnels")
+            j = r.json()
+            if "tunnels" in j:
+                try:
+                    self.brain.api_url = j["tunnels"][0]["public_url"]
+                    self.save()
+                    os.environ["SIDEBAR_API_URL"] = self.brain.api_url
+                    puts(" found at %s." % self.brain.api_url)
+
+                    if os.path.isfile(".env"):
+                        puts("Updating .env file...", newline=False)
+                        with open(".env", "r+") as f:
+                            contents = f.read()
+                            new_contents = ""
+                            for l in contents.split("\n"):
+                                if "SIDEBAR_API_URL" not in l:
+                                    new_contents += "%s\n" % l
+                            new_contents += "SIDEBAR_API_URL=%s\n" % self.brain.api_url
+                            f.seek(0)
+                            f.truncate()
+                            f.write(new_contents)
+                            puts(" done.")
+                    return 
+                except:
+                    retries += 1
+
+        puts("Failed to set tunnel.  Output: %s" % j)
 
     def post_default(self, *args, **kwargs):
         if self.brain.api_url:
